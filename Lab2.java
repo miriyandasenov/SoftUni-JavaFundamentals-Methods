c397743183d28877d58c31398a83564e30ca2867
miriyandasenov <63752573+miriyandasenov@users.noreply.github.com>
2020-05-04 20:07:00 +0300

    Grades

---
+++ Lab2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lab2
+{
+	public static void printInWords(double grade){
+		if( grade >= 2.00 && grade <= 2.99 ) System.out.println("Fail"); 
+		else if( grade >= 3.00 && grade <= 3.49 ) System.out.println("Poor");
+		else if( grade >= 3.50 && grade <= 4.49 ) System.out.println("Good");
+		else if( grade >= 4.50 && grade <= 5.49 ) System.out.println("Very good");
+		else System.out.println("Excellent");
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double number = Double.parseDouble(sc.nextLine());
+
+		printInWords(number);
+	}
+}
